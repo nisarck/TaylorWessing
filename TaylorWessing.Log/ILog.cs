4caf57f689588d5e1fe3f8275683d02734770539
Nisar Chenganakkattil <nisarck@gmail.com>
2024-11-14 17:48:39 +0000

    added log file

---
+++ TaylorWessing.Log/ILog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaylorWessing.Log
+{
+    public interface ILog
+    {
+        void Info(object message);
+        void Debug(object message);
+        void Error(object message, Exception exception);
+        void Fatal(object message);
+    }
+}
---
+++ TaylorWessing.Log/Log.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using log4net;
+using log4net.Config;
+
+
+namespace TaylorWessing.Log
+{
+    public class Log : ILog
+    {
+        log4net.ILog logger;
+
+        public Log(Type type)
+        {
+            logger = LogManager.GetLogger(type);
+        }
+        public void Info(object message)
+        {
+            logger.Info(message);
+        }
+        public void Debug(object message)
+        {
+            logger.Debug(message);
+        }
+        public void Error(object message, Exception exception)
+        {
+            logger.Error(message, exception);
+        }
+        public void Fatal(object message)
+        {
+            logger.Fatal(message);
+        }
+    }
+}
